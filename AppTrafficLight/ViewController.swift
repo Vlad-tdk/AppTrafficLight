9b5d3d86833eb90cf517aff8503508659f136f22
Vladimir <xalif80@gmail.com>
2023-04-30 13:20:03 +0200

    Initial Commit

---
+++ AppTrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTrafficLight
+//
+//  Created by Vlad on 30.4.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
